ece65de8deb1bf0e96454981aec04612e172762c
张探琛 <zhangtanchen@zhangtanchendeMacBook-Pro.local>
2020-09-27 20:15:10 +0800

    d

--- test.js
+++ test.js
@@ -4,10 +4,34 @@ Object.keys(data).forEach((i, index) => {
         searchStr += '&';
     }
     searchStr += (`${i}=${data[i]}`);
+    Object.keys(data).forEach((i, index) => {
+        if (index !== 0) {
+            searchStr += '&';
+        }
+        searchStr += (`${i}=${data[i]}`);
+    });
+    Object.keys(data).forEach((i, index) => {
+        if (index !== 0) {
+            searchStr += '&';
+        }
+        searchStr += (`${i}=${data[i]}`);
+    });
 });
 Object.keys(data).forEach((i, index) => {
     if (index !== 0) {
         searchStr += '&';
     }
     searchStr += (`${i}=${data[i]}`);
+    Object.keys(data).forEach((i, index) => {
+        if (index !== 0) {
+            searchStr += '&';
+        }
+        searchStr += (`${i}=${data[i]}`);
+    });
+    Object.keys(data).forEach((i, index) => {
+        if (index !== 0) {
+            searchStr += '&';
+        }
+        searchStr += (`${i}=${data[i]}`);
+    });
 });
