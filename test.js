6fb447b8731ae76f2f76a546f7cb4b375ee87609
张探琛 <zhangtanchen@zhangtanchendeMacBook-Pro.local>
2020-09-27 20:16:48 +0800

    a

--- test.js
+++ test.js
@@ -1,37 +1,15 @@
 let a = [1,2,3];
-Object.keys(data).forEach((i, index) => {
-    if (index !== 0) {
-        searchStr += '&';
-    }
-    searchStr += (`${i}=${data[i]}`);
-    Object.keys(data).forEach((i, index) => {
-        if (index !== 0) {
-            searchStr += '&';
-        }
-        searchStr += (`${i}=${data[i]}`);
-    });
-    Object.keys(data).forEach((i, index) => {
-        if (index !== 0) {
-            searchStr += '&';
-        }
-        searchStr += (`${i}=${data[i]}`);
-    });
-});
-Object.keys(data).forEach((i, index) => {
-    if (index !== 0) {
-        searchStr += '&';
-    }
-    searchStr += (`${i}=${data[i]}`);
-    Object.keys(data).forEach((i, index) => {
-        if (index !== 0) {
-            searchStr += '&';
-        }
-        searchStr += (`${i}=${data[i]}`);
-    });
-    Object.keys(data).forEach((i, index) => {
-        if (index !== 0) {
-            searchStr += '&';
-        }
-        searchStr += (`${i}=${data[i]}`);
-    });
-});
+function fun() {
+    a.forEach(item => {
+        console.log(item)
+    })
+    a.forEach(item => {
+        console.log(item)
+    })
+    a.forEach(item => {
+        console.log(item)
+    })
+    a.forEach(item => {
+        console.log(item)
+    })
+}
